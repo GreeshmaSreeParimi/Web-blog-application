11c405c814d98daf3909bdb051fe1805f9d9b8a2
Greeshma Sree Parimi <sree1159@uw.edu>
2022-12-20 11:34:13 -0800

    adding different pages for web site

---
+++ my-blog/src/pages/AboutPage.js
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+
+    return (
+    <h1>This is the About Page !</h1>
+    );
+
+}
+
+export default AboutPage;
---
+++ my-blog/src/pages/ArticlesListPage.js
@@ -0,0 +1,9 @@
+const ArticleListPage = () => {
+
+    return (
+    <h1>This is the Article List Page !</h1>
+    );
+
+}
+
+export default ArticleListPage;
