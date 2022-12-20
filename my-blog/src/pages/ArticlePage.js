7dadf97d908e42f3f7575504c52e0b6f50c1fa46
Greeshma Sree Parimi <sree1159@uw.edu>
2022-12-20 12:23:23 -0800

    adding articles and adding routes to articles

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,7 +1,23 @@
-const ArticlePage = () => {
+import articles from "./article-content";
+import { useParams } from "react-router-dom";
 
+const ArticlePage = () => {
+    const params = useParams();
+    const articleId = params.articleId;
+    // const {articleId} = useParams(); equals to abve two lines of code . Object destructring
+
+    const article = articles.find(article => article.name === articleId);
+
+    // react fragment --- <></> --> empty tags to display two or more tags at same time.
     return (
-    <h1>This is the Article Page !</h1>
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph,index) => (
+                <p key ={index}>{paragraph}</p>
+            ))}
+        </>
+    
+
     );
 
 }
--- my-blog/src/pages/ArticlesListPage.js
+++ my-blog/src/pages/ArticlesListPage.js
@@ -1,7 +1,21 @@
+import articles from "./article-content";
+import { Link } from "react-router-dom";
+
 const ArticleListPage = () => {
 
     return (
-    <h1>This is the Article List Page !</h1>
+        <>
+            <h1>Articles</h1>
+            {articles.map(article =>(
+                // to={`/articles/`+article.name} this and below implemetation are same.
+                <Link key= {article.name} className="article-list-item" to={`/articles/${article.name}`}>
+                    <h3>{article.title}</h3>
+                    {/* below line gets article's first paragraph first 150 characters */}
+                    <p>{article.content[0].substring(0,150)}...</p> 
+                </Link>
+            ))}
+        </>
+    
     );
 
 }
